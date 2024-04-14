76b081da4f15a0352f1ee11f55d942814c186ec4
Akash <145562031+Akash-Jakkapalli@users.noreply.github.com>
2024-04-14 07:19:08 +0000

    dataTypes

---
+++ basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 2003
+let accountEmail = "akash123@gmail.com"
+accountCity = "pune"
+var accountPassword = "0215"
+
+//accountId 2 is //not allowed
+
+ accountEmail = "akash123@gmail.com"
+ accountPassword = "0215"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity ]);
+/*
+prefer not to use var
+becouse of issue in block scope and functional scope
+*/
---
+++ basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+
+
+
+let name = "Akash"//string dataType
+let age = 19 //num
+let isLoggedIn = true //boolean
+
+// Number => 2 to pow 53
+// 
+// string => " "
+// boolean => true/false
+// null => standalne value
+let state;
+// undefined => we not given value 
+// symbol => unique
+
+//object =>
+
+console.log(typeof age);
+
+console.log(typeof undefined); // out is undefined
+console.log(typeof null); // out is object
