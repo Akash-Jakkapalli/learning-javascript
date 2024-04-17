575985b8f362282713f05cb99c6b2aa43ee8a316
Akash <145562031+Akash-Jakkapalli@users.noreply.github.com>
2024-04-17 07:29:40 +0000

    Conversion of datatypes

---
+++ basics/03_ConversionOprestion.js
@@ -0,0 +1,23 @@
+let score = 33
+
+//onsole.log(typeof score);
+
+let ValueInNumber = Number(score)
+//console.log(typeof ValueInNumber);
+//console.log(ValueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+// true => 1
+// false => 0
+
+let isLoggedIn = "33"
+
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+console.log( BooleanIsLoggedIn);
+console.log(typeof isLoggedIn);
+
+// 1 -> true / 0 -> false
+// "" -> false 
+// "Akash" -> true
