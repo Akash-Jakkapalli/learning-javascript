da42726f6fae57f0dc6e98898f2c2265d1e4ba88
Akash <145562031+Akash-Jakkapalli@users.noreply.github.com>
2024-12-01 12:07:00 +0000

    camparision

--- basics/04_camparision.js
+++ basics/04_camparision.js
@@ -1,2 +1,23 @@
-console.log(2<1);
-console.log();
+// console.log(2>1);
+
+
+const Id = Symbol('123')
+const anotherId = Symbol('123')
+
+//console.log(Id === anotherId);
+
+// array, objects,functions
+
+//array
+const Numbers = [1, 2, 3, 4, 5];
+
+//obj
+let myObj = {
+    name: "akash",
+    age: 19
+}
+
+//funtions
+const myFunction = function() {
+    console.log("akash");
+}
