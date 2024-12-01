0de572037ad4eec6c246bd9a32b4ee2bb08ba640
Akash <145562031+Akash-Jakkapalli@users.noreply.github.com>
2024-12-01 09:35:16 +0000

    dataTypes and converaion Option

--- basics/02_dataTypes.js
+++ basics/02_dataTypes.js
@@ -3,18 +3,23 @@ let name = "Akash" //string dataType
 let age = 19  //num
 let isLoggedIn = true  //boolean
 
-// Number => 2 to pow 53
-// 
+// Primitive-data types in JS
+// Number => 2 to power53
+// bigInt 
 // string => " "
 // boolean => true/false
 // null => standalne value
+
 let state;
 // undefined => we not given value 
+
 // symbol => unique
 
-//object =>
+
+
+//object 
 
 console.log(typeof age);
 
-console.log(typeof undefined); // out is undefined
-console.log(typeof null); // out is object
+console.log(typeof undefined); // output is undefined
+console.log(typeof null); // output is object
--- basics/03_ConversionOprestion.js
+++ basics/03_ConversionOprestion.js
@@ -1,10 +1,12 @@
-let score = 33
+let score = "33"
 
-//onsole.log(typeof score);
+// console.log(typeof score);
 
 let ValueInNumber = Number(score)
 //console.log(typeof ValueInNumber);
 //console.log(ValueInNumber);
+
+// NOTE:- output
 
 // "33" => 33
 // "33abc" => NaN
@@ -12,24 +14,42 @@ let ValueInNumber = Number(score)
 // true => 1
 // false => 0
 
-let isLoggedIn = "33"
+
+let isLoggedIn = 1
 
 let BooleanIsLoggedIn = Boolean(isLoggedIn)
-console.log( BooleanIsLoggedIn);
-console.log(typeof isLoggedIn);
+// console.log( BooleanIsLoggedIn);
+// console.log(typeof isLoggedIn);
 
+// NOTE :- output
 // 1 -> true / 0 -> false
 // "" -> false 
 // "Akash" -> true
 
-/*
-    Oeration
-*/
+
+
+let someNumber = 2
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+/********** Operations *********/
+
 let value = 10
 let negvalue= -value
 //console.log(-value);
 
+
+
 let str1 = "Hello"
 let str2 = " Akash"
+//console.log(str1+str2);
 
-console.log(str1+str2);
+
+// Increment
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+
