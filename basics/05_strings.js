0e063e894fb639ec761e04589f2eca53d0d62a8a
Akash <145562031+Akash-Jakkapalli@users.noreply.github.com>
2024-12-02 11:58:56 +0000

    String

---
+++ basics/05_strings.js
@@ -0,0 +1,18 @@
+const name = "Akash"
+const repoCount = 2
+
+//console.log(`hello my name is ${name} and my repo Count is ${repoCount}`); // string Interpolation
+
+// declare String
+
+const myName = "akash"
+const myLastNmae = new String(`Jakkapalli`)
+
+//console.log(myName);
+console.log(myLastNmae.length);
+
+console.log(myLastNmae.toUpperCase());
+
+
+
+
