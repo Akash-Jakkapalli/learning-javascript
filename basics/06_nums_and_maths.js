7bb01ae80f34528e768ee1c3c66df2ca05bf7cb8
Akash <145562031+Akash-Jakkapalli@users.noreply.github.com>
2024-12-02 13:42:58 +0000

    math and num

--- basics/06_nums_and_maths.js
+++ basics/06_nums_and_maths.js
@@ -34,11 +34,12 @@ const hundreds = 1000000
 
 
 console.log(Math.random());
-console.log(Math.random());
+console.log(Math.floor(Math.random() * 10) + 1 );
 
 
-
-
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1) + min))
 
 
 
