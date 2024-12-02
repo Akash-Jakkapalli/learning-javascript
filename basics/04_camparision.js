7fd475bc1ec465c4a8dc7f337691405f130cbb8f
Akash <145562031+Akash-Jakkapalli@users.noreply.github.com>
2024-12-02 09:11:14 +0000

    memory type

--- basics/04_camparision.js
+++ basics/04_camparision.js
@@ -21,3 +21,31 @@ let myObj = {
 const myFunction = function() {
     console.log("akash");
 }
+
+
+
+//-------------------------------------------------------
+
+// memory type 
+// 1st stack -> (primitive)
+// 2nd heap -> (Non-primitive)
+
+
+let myname = "Akash"
+
+let anotherName = myname
+anotherName = "SKY"
+
+console.log(myname)
+console.log(anotherName);
+
+let userOne = {
+    email: "google@.com",
+    upi: "user@hdfc"
+}
+
+let userTwo = userOne
+
+userTwo.email = "akash@gmail.com"
+
+console.log(userTwo);
