10bfd80b81acc2390754ddcfb620b5857d766065
Akash <145562031+Akash-Jakkapalli@users.noreply.github.com>
2024-12-02 14:18:35 +0000

    dates

---
+++ basics/07_datesinJS.js
@@ -0,0 +1,17 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+
+
+let creatDate = new Date(2024,11,2)
+// console.log(creatDate.toString());
+
+
+let mycreatedDate = new Date("05-02-2003")
+// console.log(mycreatedDate.toLocaleDateString());
+
