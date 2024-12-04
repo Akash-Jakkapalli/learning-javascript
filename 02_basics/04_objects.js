751cd6d0e78043be5d1e2b12c92daf4a7d35b292
Akash <145562031+Akash-Jakkapalli@users.noreply.github.com>
2024-12-04 22:52:02 +0530

    Add files via upload

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,13 @@
+
+
+// Object Distructure
+
+const cource = {
+    courceName: "JS learning",
+    prise: 199,
+    courceinstractor: "Vijay"
+}
+
+const {courceName : name} = cource
+//console.log(courceName);
+console.log(name);
