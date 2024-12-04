9d06fabbb80b25e0bd2aeb0c3cdc0579bfd1a17e
Akash <145562031+Akash-Jakkapalli@users.noreply.github.com>
2024-12-04 04:31:21 +0000

    basic objects

--- 02_basics/02_objects.js
+++ 02_basics/02_objects.js
@@ -1,9 +1,19 @@
 //Objects
 
-let user = {
+const user = {
     name:"akash",
     age: 19,
     email: "akash@google.com"
 }
+const marks = {
+    math: 99,
+    phy: 90,
+    chem: 85
+}
+// console.log(typeof user["name"]);
 
-console.log(typeof user["name"]);
+const userInfo = Object.assign(user,marks)
+console.log(userInfo);
+
+console.log(userInfo === user); //true
+
