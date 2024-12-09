e290239658cd6e6b492ea32ab0695bdf26261069
Akash <145562031+Akash-Jakkapalli@users.noreply.github.com>
2024-12-09 10:33:08 +0000

    truthy basic

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,25 @@
+// Switch
+
+const month = 3
+
+switch (3) {
+    case 1:
+        console.log("janury");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("mar");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    case 5:
+        console.log("may");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,8 @@
+const userEmail = "akash@goole.com"
+
+if (userEmail) {
+    console.log("got user Email");
+}
+else {
+    console.log("Don't have user Email");   
+}
