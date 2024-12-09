30369f565fcb9f576dc8e4daa979556116fb9c28
Akash <145562031+Akash-Jakkapalli@users.noreply.github.com>
2024-12-09 14:21:04 +0000

    basic loop's

---
+++ 05_iteration.js/two.js
@@ -0,0 +1,25 @@
+
+
+// while
+
+let i = 1
+while (i <= 10) {
+    // console.log(i);
+    i++
+}
+
+let mirzapur = ["Guddu Pandit", "Akhandanand Tripathi", "Munna ", "Dimpy Pandit", "Zarina"]
+
+let arr = 0
+while (arr < mirzapur.length) {
+    //console.log(mirzapur[arr]);
+    arr = arr+1
+    
+}
+
+let score = 1
+
+do {
+    console.log(`score is : ${score}`);
+    score++
+} while (score <= 10);
