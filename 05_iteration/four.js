81ac41db20e1dac9988e3758daeb40f7ffc74fcc
Akash <145562031+Akash-Jakkapalli@users.noreply.github.com>
2024-12-10 02:53:42 +0000

    for in using object and arr

---
+++ 05_iteration/four.js
@@ -0,0 +1,21 @@
+
+// for in loop using Object
+
+const myObject = {
+    js : 'javascript',
+    cpp : 'C++',
+    rb : 'ruby'
+ 
+}
+for (const key in myObject) {
+    console.log(`${key} :- ${myObject[key]}`);
+    
+}
+
+// for in loop using array
+
+const myarr = ['js', 'cpp,', 'rb']
+
+for (const key in myarr) {
+    
+}
